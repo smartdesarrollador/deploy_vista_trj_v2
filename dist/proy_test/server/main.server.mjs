8d24824cb3ca6088bf6b02dad5b9046846e699be
Jeans <sistemadesignstyle@gmail.com>
2025-09-11 15:42:02 -0500

    correccion url imagen

--- dist/proy_test/server/main.server.mjs
+++ dist/proy_test/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ABUOHLYU.mjs";import"./chunk-LYD6B27T.mjs";import"./chunk-DVMAORCV.mjs";import"./chunk-VGSL6YLM.mjs";import"./chunk-CIE2ZWBG.mjs";import"./chunk-YEWAR4WW.mjs";import"./chunk-D7ZWPOEP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-TFSXF2E5.mjs";import"./chunk-LYD6B27T.mjs";import"./chunk-DVMAORCV.mjs";import"./chunk-VGSL6YLM.mjs";import"./chunk-CIE2ZWBG.mjs";import"./chunk-YEWAR4WW.mjs";import"./chunk-D7ZWPOEP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
