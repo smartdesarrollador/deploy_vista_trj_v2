c3c972d1fb6f9f49575497407fad39681fb59930
Jeans <sistemadesignstyle@gmail.com>
2025-09-12 02:33:04 -0500

    correccion pwa movil

--- dist/proy_test/server/main.server.mjs
+++ dist/proy_test/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-LLJ3Q53Q.mjs";import"./chunk-6L7RUOXU.mjs";import"./chunk-2MZN33M3.mjs";import"./chunk-7VIZGLJ6.mjs";import"./chunk-WBUT2LNE.mjs";import"./chunk-MJPLGBXN.mjs";import"./chunk-EYPCSYAF.mjs";import"./chunk-D7ZWPOEP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-LOPPNUYQ.mjs";import"./chunk-6L7RUOXU.mjs";import"./chunk-2MZN33M3.mjs";import"./chunk-7VIZGLJ6.mjs";import"./chunk-WBUT2LNE.mjs";import"./chunk-MJPLGBXN.mjs";import"./chunk-EYPCSYAF.mjs";import"./chunk-D7ZWPOEP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
