c122168e027ff024b904947c297b6f009509423c
Jeans <sistemadesignstyle@gmail.com>
2025-09-12 01:49:52 -0500

    configurando pwa

--- dist/proy_test/server/main.server.mjs
+++ dist/proy_test/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-MBCO4ZF7.mjs";import"./chunk-LYD6B27T.mjs";import"./chunk-2GMHDPLF.mjs";import"./chunk-YD2M4XOO.mjs";import"./chunk-WBUT2LNE.mjs";import"./chunk-CIE2ZWBG.mjs";import"./chunk-YEWAR4WW.mjs";import"./chunk-D7ZWPOEP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-CQZVFEPU.mjs";import"./chunk-6L7RUOXU.mjs";import"./chunk-2MZN33M3.mjs";import"./chunk-7VIZGLJ6.mjs";import"./chunk-WBUT2LNE.mjs";import"./chunk-MJPLGBXN.mjs";import"./chunk-EYPCSYAF.mjs";import"./chunk-D7ZWPOEP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
